dc12cd49b98127f687fb275525b60b3c77de465f
JordanBoswell <jordanboswell1190@gmail.com>
2014-05-28 02:57:50 -0400

    Initial commit.

---
+++ Include/SoundPlayer.h
@@ -0,0 +1,41 @@
+#ifndef SoundPlayer_h
+#define SoundPlayer_h
+
+
+
+#include <list>
+#include <SFML\Audio\Sound.hpp>
+#include <SFML\System\Vector3.hpp>
+
+
+//P.O.D. that contains all information needed to specify a sound's properties.
+struct SoundInfo
+{
+    bool relativeToListener{true};
+    sf::Vector3f position{0.0f, 0.0f, 0.0f};
+    float volume{100.0f};
+    float pitch{1.0f};
+    float minDistance{200.0f};
+    float attenuation{8.0f};
+};
+
+
+/*----------------------------------------------------------------------------------
+Manager and player of sounds.  Sounds must be periodically removed via a call to 
+removeStoppedSounds in order to stay under the limit of simultaneous sounds and music.
+The sounds can be positioned in 3d space.
+ ----------------------------------------------------------------------------------*/
+class SoundPlayer : sf::NonCopyable
+{
+public:
+    void playSound(sf::SoundBuffer& soundBuffer, SoundInfo soundInfo = SoundInfo());
+    //Should be called periodically(though not necessarily each frame) to remove finished sounds
+    void removeStoppedSounds();
+    
+private:
+    std::list<sf::Sound> mSounds;
+};
+
+
+
+#endif
---
+++ Source/SoundPlayer.cpp
@@ -0,0 +1,32 @@
+#include <cassert>
+#include "SoundPlayer.h"
+
+
+
+void SoundPlayer::playSound(sf::SoundBuffer& soundBuffer, SoundInfo soundInfo)
+{
+    assert(soundInfo.volume >= 0 && soundInfo.volume <= 100 && "Volume must be between 0-100 in SoundPlayer::playSound()");
+    assert(soundInfo.pitch > 0 && "Pitch must be greater than 0 in SoundPlayer::playSound");
+    assert(soundInfo.minDistance > 0 && "minDistance must be greater than 0 in SoundPlayer::playSound");
+    assert(soundInfo.attenuation >= 0 && "attenuation must be greater than or equal to 0 in SoundPlayer::playSound");
+    
+    sf::Sound sound(soundBuffer);
+    sound.setRelativeToListener(soundInfo.relativeToListener);
+    sound.setPosition(soundInfo.position);
+    sound.setVolume(soundInfo.setVolume);
+    sound.setPitch(soundInfo.setPitch);
+    sound.setMinDistance(soundInfo.minDistance);
+    sound.setAttentuation(soundInfo.attenuation);
+    mSounds.push_back(sf::Sound(soundBuffer))
+    mSounds.back().play();
+}
+
+
+void SoundPlayer::removeStoppedSounds()
+{
+    for(auto iter = mSounds.begin(); iter != mSounds.end(); ++iter)
+    {
+        if(iter->getStatus() == sf::Sound::Status::Stopped)
+            mSounds.erase(iter);
+    }
+}
